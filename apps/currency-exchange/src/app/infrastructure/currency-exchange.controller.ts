37bd345270a916e232a9571cf45eccbaef6f86cf
Eduardo Ochoa <edochoas@falabella.cl>
2023-09-12 20:03:13 -0300

    feat: OpenAPI configurations (swagger)

--- apps/currency-exchange/src/app/infrastructure/currency-exchange.controller.ts
+++ apps/currency-exchange/src/app/infrastructure/currency-exchange.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Get, Header, Query } from '@nestjs/common';
-
 import { CurrencyExchangeService } from '../application/currency-exchange.service';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: CurrencyExchangeService) {}
 
+  @ApiQuery({
+    name: 'amount',
+    description: 'amount to exchage'
+  })
   @Get('/exchange')
   @Header(
     'Content-Disposition',
     'attachment; filename="transaction-details.csv'
   )
-  getData(@Query('amount') amount) {
-    return this.appService.calculateBestExchangeRates(amount);
+  getData(@Query('amount') amount: string) {
+    const amountNumber = Number(amount);
+    return this.appService.calculateBestExchangeRates(amountNumber);
   }
 }
--- apps/currency-exchange/src/main.ts
+++ apps/currency-exchange/src/main.ts
@@ -1,22 +1,40 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common';
+import { INestApplication, Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+const GLOBAL_PREFFIX = 'currency-exchange';
 
 async function bootstrap() {
+  const app = await createApp();
+  setupSwagger(app);
+  setupServer(app);
+}
+
+async function createApp() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'currency-exchange';
-  app.setGlobalPrefix(globalPrefix);
+  app.setGlobalPrefix(GLOBAL_PREFFIX);
+  return app
+}
+
+async function setupServer(app: INestApplication) {
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFFIX}`
   );
+}
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Currency Exchange')
+    .setDescription(
+      'API that allows you to calculate the best conversion of the Canadian dollar to other currencies'
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(GLOBAL_PREFFIX, app, document);
 }
 
 bootstrap();

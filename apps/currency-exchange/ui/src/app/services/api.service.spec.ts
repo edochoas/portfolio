4f031ba6bed13937162dda9092f0ce2521cbb210
Eduardo Ochoa <edochoas@falabella.cl>
2024-07-15 12:14:44 -0400

    test(currency-exchange-ui): add unit test for api service

--- apps/currency-exchange/ui/src/app/services/api.service.spec.ts
+++ apps/currency-exchange/ui/src/app/services/api.service.spec.ts
@@ -1,16 +1,93 @@
 import { TestBed } from '@angular/core/testing';
-
-import { ApiService } from './api.service';
+import { ApiService, DEFAULT_ERROR } from './api.service';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpTesting: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(ApiService);
+    httpTesting = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTesting.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch the currencies', (done) => {
+    const mockCurrencies = [
+      {
+        "currencyName": "Hong Kong Dollar",
+        "currencyCode": "HKD"
+      },
+      {
+        "currencyName": "USA Dollar",
+        "currencyCode": "USD"
+      }
+    ];
+
+    service.getCurrencies().subscribe((currencies) => {
+      expect(currencies.length).toBe(2);
+      expect(currencies).toEqual(mockCurrencies);
+      done();
+    });
+
+    const mockHttp = httpTesting.expectOne('/currency-exchange-api/currencies');
+    expect(mockHttp.request.method).toBe('GET');
+    mockHttp.flush(mockCurrencies);
+  });
+
+  it('should convert the currency', (done) => {
+    const amount = 100;
+    const currency = 'EUR';
+    const mockConversion = {
+      amount: 67.18673778126583,
+      path: [
+        {
+          previousAmount: amount,
+          previousCurrency: 'CAD',
+          currencyCode: currency,
+          exchangeRate: 0.6718673778126584,
+          convertedAmount: 67.18673778126583
+        }
+      ]
+    };
+
+    service.convert(amount, currency).subscribe((conversion) => {
+      expect(conversion).toEqual(mockConversion);
+      done();
+    });
+
+    const mockHttp = httpTesting.expectOne(`/currency-exchange-api/convert?amount=${amount}&currency=${currency}`);
+    expect(mockHttp.request.method).toBe('GET');
+    mockHttp.flush(mockConversion);
+  });
+
+  it('should handle the error', (done) => {
+    const amount = 100;
+    const currency = 'EUR';
+    service.convert(amount, currency).subscribe({
+      next: () => fail('should have failed with an error'),
+      error: (error) => {
+        expect(error?.message).toEqual(DEFAULT_ERROR);
+        done()
+      }
+    });
+
+    const mockHttp = httpTesting.expectOne(`/currency-exchange-api/convert?amount=${amount}&currency=${currency}`);
+    expect(mockHttp.request.method).toBe('GET');
+    mockHttp.flush('Error', { status: 500, statusText: 'Internal Server error' });
+  })
 });
--- apps/currency-exchange/ui/src/app/services/api.service.ts
+++ apps/currency-exchange/ui/src/app/services/api.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 
 export type Transaction = {
   previousAmount: number;
@@ -10,7 +10,7 @@ export type Transaction = {
   convertedAmount: number;
 };
 
-type ConvertResponse = {
+type Conversion = {
   amount: number;
   path: Transaction[];
 };
@@ -18,6 +18,8 @@ export interface Currency {
   currencyName: string;
   currencyCode: string;
 }
+
+export const DEFAULT_ERROR = 'An error occurred; please try again later.';
 
 @Injectable({
   providedIn: 'root',
@@ -28,12 +30,21 @@ export class ApiService {
   constructor(private http: HttpClient) {}
 
   getCurrencies() {
-    return this.http.get<Currency[]>(`/${this.basePath}/currencies`);
+    return this.http
+      .get<Currency[]>(`/${this.basePath}/currencies`)
+      .pipe(catchError(this.handleError));
   }
 
-  convert(amount: number, currency: string): Observable<ConvertResponse> {
-    return this.http.get<ConvertResponse>(
+  convert(amount: number, currency: string): Observable<Conversion> {
+    return this.http.get<Conversion>(
       `/${this.basePath}/convert?amount=${amount}&currency=${currency}`
+    ).pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    console.error(error);
+    return throwError(
+      () => new Error(DEFAULT_ERROR)
     );
   }
 }
